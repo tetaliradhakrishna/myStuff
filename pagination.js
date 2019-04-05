cc165eaef59f5c14d3907d2447691f20f9088bef
radhakrishna <radha784578@gmail.com>
2019-04-05 13:09:42 +0530

    changes

---
+++ pagination.js
@@ -0,0 +1,40 @@
+let CLOUDANT_HOST_SSL_PROD = process.env.CLOUDANT_HOST_SSL_PROD;
+     //console.log("process",CLOUDANT_HOST_SSL_PROD);
+let page_result = 10;
+let skip_rows = 1;
+let url =  CLOUDANT_HOST_SSL_PROD + '/sslwarehouse/'  + '_design/sslDesignDoc/_search/byClientName?include_docs=true';
+		
+    // work on this procees for pagination and queies
+    // integrated limit option  based on the  given number records will be fetch
+    // what is skip  
+    url = url + '&query=client:\"' + 'DECCAN FINE CHEMICALS (INDIA) PVT LTD' + '\"' 
+              + '&skip=' + skip_rows 
+              + '&limit=' + page_result;
+
+let http = require('request');
+   basedOnthePagenation();
+
+
+   function basedOnthePagenation(){
+      
+       http({
+       	   uri:url,
+           method:'GET',    
+           headers: 'Content-Type: application/json,charset:utf8'
+       },(err,res,body)=>{
+                if(err){
+                	console.log(err);
+                }
+                let pusded_Date = [];
+                console.log('Res',res.statusCode);
+                console.log('Total_Rows',JSON.parse(body).total_rows);
+                console.log('Book_Mark',JSON.parse(body).bookmark);
+                console.log('BODY_LIMIT_LENGTH',JSON.parse(body).rows.length);
+                
+                for(let i=0;i<JSON.parse(body).rows.length;i++){
+                    pusded_Date.push(JSON.parse(body).rows[i].doc);
+                }
+               console.log('pusded_Data',pusded_Date[0]);
+       })
+   
+   };                        
