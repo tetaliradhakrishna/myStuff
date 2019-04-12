93ffdc06b84633431e3e68d9a824eca6230f84e7
radhakrishna <radha784578@gmail.com>
2019-04-12 17:51:35 +0530

    process secuirtiy

--- yearBaseSearch.js
+++ yearBaseSearch.js
@@ -2,14 +2,15 @@ let CLOUDANT_HOST_SSL_PROD = process.env.CLOUDANT_HOST_SSL_PROD;
      //console.log("process",CLOUDANT_HOST_SSL_PROD);
 let page_result = 10;
 let skip_rows = 1;
-let url =  CLOUDANT_HOST_SSL_PROD + '/sslwarehouse/'  + '_design/sslDesignDoc/_search/byClientName?include_docs=true';
+let url =  CLOUDANT_HOST_SSL_PROD + '/sslwarehouse/'  + '_design/sslDesignDoc/_search/businessYear?include_docs=true';
 		
     // work on this procees for pagination and queies
     // integrated limit option  based on the  given number records will be fetch
     // what is skip  
-    url = url + '&query=client:\"' + process.env.CLIENT_NAME + '\"' 
-              + '&skip=' + skip_rows 
-              + '&limit=' + page_result;
+    url = url + '&query=businessYear:\"' + '2017-2018' + ' \"' + 'AND client:' +  process.env.CLIENT_NAME
+  
+  //url = url + '&query=client:\"' + process.env.CLIENT_NAME + ' \"'  
+        
 
 let http = require('request');
    basedOnthePagenation();
@@ -28,13 +29,13 @@ let http = require('request');
                 let pusded_Date = [];
                 console.log('Res',res.statusCode);
                 console.log('Total_Rows',JSON.parse(body).total_rows);
-                console.log('Book_Mark',JSON.parse(body).bookmark);
+                //console.log('Book_Mark',JSON.parse(body).bookmark);
                 console.log('BODY_LIMIT_LENGTH',JSON.parse(body).rows.length);
                 
                 for(let i=0;i<JSON.parse(body).rows.length;i++){
                     pusded_Date.push(JSON.parse(body).rows[i].doc);
                 }
-               console.log('pusded_Data',pusded_Date[0]);
+               //console.log('pusded_Data',pusded_Date[0]);
        })
    
    };                        
