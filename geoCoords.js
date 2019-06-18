ac8a11b5a27e4e4b0bb73991b26d850117f776eb
radhakrishna <radhakrishna@whiznext.com>
2019-06-18 14:34:18 +0530

    changes

--- geoCoords.js
+++ geoCoords.js
@@ -5,7 +5,7 @@ var options = {
 
     // Optional depending on the providers
     httpAdapter: 'https', // Default
-    apiKey: 'AIzaSyBmd8kr7HfBVdgUINdV0C9miPBD8z-8jL4', // for Mapquest, OpenCage, Google Premier
+    apiKey: '', // for Mapquest, OpenCage, Google Premier
     formatter: null         // 'gpx', 'string', ...
 };
 
